d5c539289bf59439877ca0b1a893f6d82bab703b
Andrea Marini <andrea.marini@cern.ch>
2014-11-07 14:12:08 +0100

    reduce n. of bins in hv

--- src/PlotAnalysis.cpp
+++ src/PlotAnalysis.cpp
@@ -12,8 +12,8 @@ void PlotAnalysis::Init(LoopAndFill *l1)
 		l->BookHisto(Form("tprofile_chint_sub_ch%d_E%d",iCh,E[i]),"ch_int_sub vs HV",2000,-0.5,1999.5,"TProfile");
 		l->BookHisto(Form("tprofile_maxampl_sub_ch%d_E%d",iCh,E[i]),"max_ampl_sub vs HV",2000,-0.5,1999.5,"TProfile");
 		// --- BOOKING THE HISTO THEMSELVEL TO HAVE BETTER RESULTS
-		l->BookHisto(Form("th2d_chint_sub_ch%d_E%d",iCh,E[i])," ch_int_sub vs HV",5000,0-50.,500000-50.,2000,-0.5,1999.5,"TH2D");
-		l->BookHisto(Form("th2d_maxampl_sub_ch%d_E%d",iCh,E[i])," max_ampl_sub vs HV",4000,-0.5,3999.5,2000,-0.5,1999.5,"TH2D");
+		l->BookHisto(Form("th2d_chint_sub_ch%d_E%d",iCh,E[i])," ch_int_sub vs HV",5000,0-50.,500000-50.,40,0.-20,2000-20,"TH2D");
+		l->BookHisto(Form("th2d_maxampl_sub_ch%d_E%d",iCh,E[i])," max_ampl_sub vs HV",4000,-0.5,3999.5,40,0.-20.,2000-20,"TH2D");
 		}
 	cout<<"[PlotAnalysis]::[Init] Done"<<endl;
 }
